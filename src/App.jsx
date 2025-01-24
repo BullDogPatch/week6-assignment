bc17c238b329713cc09a8db6902a250a7009562a
Craig Clayton <craig__clayton@outlook.com>
2025-01-24 10:33:53 +0000

    fetch data from the cookie api

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 function App() {
   const [totalCookies, setTotalCookies] = useState(0);
   const [cps, setCps] = useState(1);
+  const [upgrades, setUpgrades] = useState([]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -11,6 +12,17 @@ function App() {
 
     return () => clearInterval(interval);
   }, [cps]);
+
+  useEffect(() => {
+    const fetchUpgrades = async () => {
+      const response = await fetch(
+        'https://cookie-upgrade-api.vercel.app/api/upgrades'
+      );
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchUpgrades();
+  }, []);
 
   return <h1>Cookie Clicker {totalCookies}</h1>;
 }
