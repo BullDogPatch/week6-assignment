75203abd865ee29cce16c9a724fd4f74b889ffd0
Craig Clayton <craig__clayton@outlook.com>
2025-01-25 22:21:10 +0000

    add github link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,18 @@
+import { FaSquareGithub } from 'react-icons/fa6';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      <p>Made with ❤️ by Craig Clayton</p>
+      <p className='made-by'>Made with ❤️ by Craig Clayton</p>
+      <a
+        href='https://github.com/BullDogPatch/week6-assignment'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='github-link'
+      >
+        <FaSquareGithub size={30} />
+      </a>
     </footer>
   );
 };
